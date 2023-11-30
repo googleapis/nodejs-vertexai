79d1fa5b88e0aa8b2a9345a9dd3c7d413314418a
Sara Robinson <sararob@google.com>
2023-11-30 12:09:18 -0800

    chore: add e2e system test
    PiperOrigin-RevId: 586748584

---
+++ system_test/end_to_end_sample_test.ts
@@ -0,0 +1,82 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// @ts-ignore
+import {VertexAI} from '@google-cloud/vertexai';
+
+const PROJECT =
+    'cloud-llm-preview1';  // TODO: change this to infer from Kokoro env
+const LOCATION = 'us-central1';
+const TEXT_REQUEST = {
+  contents: [{role: 'user', parts: [{text: 'How are you doing today?'}]}],
+};
+
+const TEXT_PART = {
+  text: 'What is this a picture of?'
+};
+// TODO: change this to an image in our test project when it's set up
+const GCS_FILE_PART = {
+  file_data: {
+    file_uri: 'gs://sararob_imagegeneration_test/kitten.jpeg',
+    mime_type: 'image/jpeg'
+  }
+};
+const MULTI_PART_REQUEST = {
+  contents: [{role: 'user', parts: [TEXT_PART, GCS_FILE_PART]}],
+};
+
+// Initialize Vertex with your Cloud project and location
+const vertex_ai = new VertexAI({project: PROJECT, location: LOCATION});
+
+const generativeTextModel = vertex_ai.preview.getGenerativeModel({
+  model: 'gemini-pro',
+});
+
+const generativeVisionModel = vertex_ai.preview.getGenerativeModel({
+  model: 'gemini-vision-pro',
+});
+
+async function testStreamGenerateContentText() {
+  const streamingResp =
+      await generativeTextModel.streamGenerateContent(TEXT_REQUEST);
+
+  for await (const item of streamingResp.stream) {
+    console.log('stream chunk:', item);
+  }
+
+  console.log('aggregated response: ', await streamingResp.response);
+}
+
+async function testStreamGenerateContentMultiPart() {
+  const streamingResp =
+      await generativeVisionModel.streamGenerateContent(MULTI_PART_REQUEST);
+
+  for await (const item of streamingResp.stream) {
+    console.log('stream chunk:', item);
+  }
+
+  console.log('aggregated response: ', await streamingResp.response);
+}
+
+async function testCountTokens() {
+  const countTokensResp = await generativeVisionModel.countTokens(TEXT_REQUEST);
+  console.log('count tokens response: ', countTokensResp);
+}
+
+testStreamGenerateContentText();
+testStreamGenerateContentMultiPart();
+testCountTokens();
