5ca46382b5f665abeebe1b62e820169c8357fee0
Yvonne Yu <yyyu@google.com>
2024-02-06 11:43:32 -0800

    chore: make top non-public countTokens function
    PiperOrigin-RevId: 604716100

---
+++ src/functions/count_tokens.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {CountTokensRequest, CountTokensResponse} from '../types/content';
+import {GoogleGenerativeAIError} from '../types/errors';
+import {
+  throwErrorIfNotOK,
+  processCountTokenResponse,
+} from './post_fetch_processing';
+import {postRequest} from './post_request';
+
+/**
+ * Make a async request to count tokens.
+ * @param request A CountTokensRequest object with the request contents.
+ * @return The CountTokensResponse object with the token count.
+ */
+export async function countTokens(
+  location: string,
+  project: string,
+  publisherModelEndpoint: string,
+  token: Promise<any>,
+  apiEndpoint: string,
+  request: CountTokensRequest
+): Promise<CountTokensResponse> {
+  const response = await postRequest({
+    region: location,
+    project: project,
+    resourcePath: publisherModelEndpoint,
+    resourceMethod: 'countTokens',
+    token: await token,
+    data: request,
+    apiEndpoint: apiEndpoint,
+  }).catch(e => {
+    throw new GoogleGenerativeAIError('exception posting request', e);
+  });
+  throwErrorIfNotOK(response);
+  return processCountTokenResponse(response);
+}
