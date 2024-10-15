a3beab1f30540749fbeba40a13327ea078dbed69
Yvonne Yu <150068659+yyyu-google@users.noreply.github.com>
2024-10-15 13:28:11 -0700

    Copybara import of the project:
    --
    f8a3bdf55b6ee694a8fd41df29bdba54d7f8cdc2 by Oleksii Oleniuk <58557113+oleksii-oleniuk-star@users.noreply.github.com>:
    
    fix: Add Context Cache support for ChatSessionPreview class (#433)
    
    * add content cache to chat session
    
    * remove logs, add gitignore
    
    ---------
    
    Co-authored-by: Oleksii Oleniuk <oleksii.olenyuk@gmail.com>
    Co-authored-by: Yvonne Yu <150068659+yyyu-google@users.noreply.github.com>
    --
    64ac1a8f9b0727ddb5e61d5319de9539fcbf8143 by Wenhao <wenhao.zhan18@gmail.com>:
    
    Fixed link in Part JSDoc
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/googleapis/nodejs-vertexai/pull/440 from WenHaozhan:fix/part-jsdoc 16bb232b5720fc35be2cb9301bdb28944ebdc4da
    PiperOrigin-RevId: 686218863

--- src/types/content.ts
+++ src/types/content.ts
@@ -459,8 +459,6 @@ export declare interface UsageMetadata {
   candidatesTokenCount?: number;
   /** Optional. Total number of tokens. */
   totalTokenCount?: number;
-  /** Optional. Number of tokens in the cached content. */
-  cachedContentTokenCount?: number;
 }
 
 /**
