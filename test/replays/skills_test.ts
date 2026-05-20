f9f48199ea5ec246d3c97032ca83e30932280314
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2026-05-20 11:07:46 -0700

    feat: Add Skill Registry UpdateSkill to SDK
    PiperOrigin-RevId: 918545724

--- test/replays/skills_test.ts
+++ test/replays/skills_test.ts
@@ -5,6 +5,7 @@
  */
 import 'jasmine';
 import {ReplayClient} from './_replay_client.js';
+import * as types from '../../src/types.js';
 describe('Skills', () => {
   let client: ReplayClient;
   beforeEach(() => {
@@ -53,6 +54,56 @@ describe('Skills', () => {
     });
     client.verifyInteraction(0, fetchSpy.calls.argsFor(0));
     expect(skill.name).toBeDefined();
+    client.verifyAllInteractions();
+  });
+
+  it('updates a skill resource with polling', async () => {
+    const fetchSpy = client.setupReplay(
+        'skills_update/test_update_skill.vertex.json');
+    await client.skills.create({
+      skillId: 'my-skill-to-update-1',
+      displayName: 'Original Skill',
+      description: 'Original Description',
+      config: {
+        zippedFilesystem: 'UEsDBBQAAAAIAAAAIQA7t4igHwAAAB0AAAAIAAAAU0tJTEwubWRTVghJLS5RCM7OzMnh8szLLMlMzFFIzs8rSc0r0QMAUEsBAhQDFAAAAAgAAAAhADu3iKAfAAAAHQAAAAgAAAAAAAAAAAAAAKQBAAAAAFNLSUxMLm1kUEsFBgAAAAABAAEANgAAAEUAAAAAAA==',
+      }
+    });
+
+    const skill = (await client.skills.update({
+      name: 'projects/682537715590/locations/us-central1/skills/my-skill-to-update-1',
+      config: {
+        displayName: 'My Updated Replay Skill',
+        description: 'My Updated Replay Skill Description',
+      }
+    })) as types.Skill;
+
+    expect(skill.displayName).toBe('My Updated Replay Skill');
+    expect(skill.description).toBe('My Updated Replay Skill Description');
+    client.verifyInteraction(0, fetchSpy.calls.argsFor(0));
+    client.verifyAllInteractions();
+  });
+
+  it('updates a skill resource using prezipped bytes with polling', async () => {
+    const fetchSpy = client.setupReplay(
+        'skills_update/test_update_skill_with_zipped_bytes.vertex.json');
+    await client.skills.create({
+      skillId: 'my-skill-to-update-2',
+      displayName: 'Original Skill',
+      description: 'Original Description',
+      config: {
+        zippedFilesystem: 'UEsDBBQAAAAIAAAAIQA7t4igHwAAAB0AAAAIAAAAU0tJTEwubWRTVghJLS5RCM7OzMnh8szLLMlMzFFIzs8rSc0r0QMAUEsBAhQDFAAAAAgAAAAhADu3iKAfAAAAHQAAAAgAAAAAAAAAAAAAAKQBAAAAAFNLSUxMLm1kUEsFBgAAAAABAAEANgAAAEUAAAAAAA==',
+      }
+    });
+
+    const skill = (await client.skills.update({
+      name: 'projects/682537715590/locations/us-central1/skills/my-skill-to-update-2',
+      config: {
+        zippedFilesystem: 'UEsDBBQAAAAAAAAAIQD9a9GnMQAAADEAAAAIAAAAU0tJTEwubWQjIE15IFVwZGF0ZWQgWmlwcGVkIFJlcGxheSBTa2lsbApUaGlzIGlzIHVwZGF0ZWQuUEsBAhQDFAAAAAAAAAAhAP1r0acxAAAAMQAAAAgAAAAAAAAAAAAAAIABAAAAAFNLSUxMLm1kUEsFBgAAAAABAAEANgAAAFcAAAAAAA==',
+      }
+    })) as types.Skill;
+
+    expect(skill.zippedFilesystem).toBe('UEsDBBQAAAAAAAAAIQD9a9GnMQAAADEAAAAIAAAAU0tJTEwubWQjIE15IFVwZGF0ZWQgWmlwcGVkIFJlcGxheSBTa2lsbApUaGlzIGlzIHVwZGF0ZWQuUEsBAhQDFAAAAAAAAAAhAP1r0acxAAAAMQAAAAgAAAAAAAAAAAAAAIABAAAAAFNLSUxMLm1kUEsFBgAAAAABAAEANgAAAFcAAAAAAA==');
+    client.verifyInteraction(0, fetchSpy.calls.argsFor(0));
     client.verifyAllInteractions();
   });
 });
